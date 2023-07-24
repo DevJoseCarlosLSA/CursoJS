9fdd2f9687ddba427e0ea1b0257e1bb1b5d7bf84
José Carlos <45600461+DevJoseCarlosLSA@users.noreply.github.com>
2023-07-24 15:26:43 -0300

    Tratamento de dados - Curso JavaScript #06

---
+++ aula04/Terminal.js
@@ -0,0 +1,31 @@
+> var n = 200
+undefined
+> n
+200
+> typeof n
+'number'
+> n = "google"
+'google'
+> typeof n
+'string'
+> typeof 6
+'number'
+> typeof 6.5
+'number'
+> typeof "6.5"
+'string'
+> typeof []
+'object'
+> typeof {}
+'object'
+> typeof function (){}
+'function'
+> typeof undefined
+'undefined'
+> typeof NaN
+'number'
+> typeof Infinity
+'number'
+> typeof null
+'object'
+>
