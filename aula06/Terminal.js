b568b5f602d35832dac44cbadf1451f86dc8d31a
José Carlos <45600461+DevJoseCarlosLSA@users.noreply.github.com>
2023-07-27 02:13:13 -0300

    Operadores (Parte 2) - Curso JavaScript #08

---
+++ aula06/Terminal.js
@@ -0,0 +1,16 @@
+var n1 =1545.5
+
+> n1
+1545.5
+> n1.toFixed(2)
+'1545.50'
+> n1.toFixed(2).replace('.',',')
+'1545,50'
+> n1.toLocaleString('pt-BR',{style: 'currency', currency: 'BRL'})
+'R$ 1.545,50'
+> n1.toLocaleString('pt-BR',{style: 'currency', currency: 'USD'})
+'US$ 1.545,50'
+> n1.toLocaleString('pt-BR',{style: 'currency', currency: 'EUR'})
+'US$ 1.545,50'
+> n1.toLocaleString('pt-BR',{style: 'currency', currency: 'EUR'})
+'US$ 1.545,50'
